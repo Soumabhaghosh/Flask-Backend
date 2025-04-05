83af717d79cd29de818fe6301b58a23985632c26
Soumabha Ghosh <soumabhaghosh@Mac-mini.local>
2025-04-05 13:48:34 +0530

    PORTfixed3

--- api.py
+++ api.py
@@ -478,4 +478,4 @@ def accountDashboard(account_name):
 if __name__ == '__main__':
     # print(os.environ.get("PORT"))
     port = int(os.environ.get("PORT"))
-    app.run(debug=True,port=port)
+    app.run(port=port)
